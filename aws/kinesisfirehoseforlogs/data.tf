b33da840b7fd00726738081651c7bce240fbb798
mfann <mfann@origamirisk.com>
2024-10-10 15:47:36 -0700

    Comment out unused AWS Serverless Application Repository data blocks

--- aws/kinesisfirehoseforlogs/data.tf
+++ aws/kinesisfirehoseforlogs/data.tf
@@ -4,7 +4,7 @@ data "aws_caller_identity" "current" {}
 
 data "sumologic_caller_identity" "current" {}
 
-data "aws_serverlessapplicationrepository_application" "app" {
-  application_id   = "arn:aws:serverlessrepo:us-east-1:956882708938:applications/sumologic-loggroup-connector"
-  semantic_version = "1.0.11"
-}
+# data "aws_serverlessapplicationrepository_application" "app" {
+#   application_id   = "arn:aws:serverlessrepo:us-east-1:956882708938:applications/sumologic-loggroup-connector"
+#   semantic_version = "1.0.11"
+# }
